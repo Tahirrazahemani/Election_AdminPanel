e39d0be5b383ecdf09d9ba8ef749a56cd27095cc
Tahir Raza Hemani <46964162+Tahirrazahemani@users.noreply.github.com>
2020-04-21 18:52:56 +0500

    Add files via upload

---
+++ IPTProject_AdminPanel/IPTProject_AdminPanel/AddArea.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IPTProject_AdminPanel
+{
+    public partial class AddArea : Form
+    {
+        public AddArea()
+        {
+            InitializeComponent();
+        }
+
+        private void AddAreaButton_Click(object sender, EventArgs e)
+        {
+            string Hosting = ConfigurationManager.AppSettings["DefineHostingRoute"];
+           
+            if (!AreaNameTextBox.Text.Equals("") && !AreaDetailTextBox.Text.Equals(""))
+            {
+                AreaClass AreaDtoObject = new AreaClass(AreaNameTextBox.Text, AreaDetailTextBox.Text);
+                HttpClient clint = new HttpClient();
+                clint.BaseAddress = new Uri(Hosting);
+                HttpResponseMessage response = clint.PostAsJsonAsync("api/areas", AreaDtoObject).Result;
+                MessageBox.Show("New Area Is Added Succefully");
+                Utils utilsObject = new Utils();
+                AreaNameTextBox.Text = "";
+                AreaDetailTextBox.Text = "";
+                utilsObject.backToAdmin(this);
+            }
+            else
+            {
+                MessageBox.Show("All Fields Should Be Filled");
+            }
+        }
+    }
+}
---
+++ IPTProject_AdminPanel/IPTProject_AdminPanel/AddParty.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IPTProject_AdminPanel
+{
+    public partial class AddParty : Form
+    {
+        public AddParty()
+        {
+            InitializeComponent();
+        }
+
+        private void AddPartyButton_Click(object sender, EventArgs e)
+        {
+            if (!PartyNameTextBox.Text.Equals("") && !PartyDetailsTextBox.Text.Equals(""))
+            {
+                string Hosting = ConfigurationManager.AppSettings["DefineHostingRoute"];
+                PartyClass PartyClassObject = new PartyClass(PartyNameTextBox.Text, PartyDetailsTextBox.Text);
+                HttpClient clint = new HttpClient();
+                clint.BaseAddress = new Uri(Hosting);
+                HttpResponseMessage response = clint.PostAsJsonAsync("api/parties", PartyClassObject).Result;
+                MessageBox.Show("New Party Is Added Succefully");
+                PartyNameTextBox.Text = "";
+                PartyDetailsTextBox.Text = "";
+                Utils utilsObject = new Utils();
+                utilsObject.backToAdmin(this);
+            }
+            else
+            {
+                MessageBox.Show("All Fields Should Be Filled");
+            }
+        }
+    }
+}
